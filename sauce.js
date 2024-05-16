68871cdf19dbe180b8e97d754ba8b27eea482aa9
smallfoot <smallfoot@osiris.cyber.nyu.edu>
2024-05-16 13:11:39 -0400

    Exported all sauce positioning code to sauce.js

---
+++ sauce.js
@@ -0,0 +1,16 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const sauce = document.querySelector('.sauce');
+
+    function updateSaucePosition() {
+        const bottomSauce = sauce.getBoundingClientRect().bottom;
+        const bottomViewport = window.innerHeight;
+        const bottomPosition = bottomSauce > bottomViewport ? 'relative' : 'absolute';
+
+        sauce.style.position = bottomPosition;
+        span.style.display = 'block';
+    }
+
+    updateSaucePosition();
+
+    window.addEventListener('resize', updateSaucePosition);
+});
