53db7b94705c8e20b6ac6855141e116eca5fa243
smallfoot <smallfoot@osiris.cyber.nyu.edu>
2025-03-24 00:17:17 -0400

    Created rankfetch.js

---
+++ rankfetch.js
@@ -0,0 +1,9 @@
+fetch("https://ctftime.org/api/v1/teams/439/")
+            .then(response => response.json())
+            .then(data => {
+                const data2025 = data["2025"];
+
+                document.getElementById("rank_national").textContent = data2025.country_place;
+                document.getElementById("rank_global").textContent = data2025.rating_place;
+            })
+            .catch(error => console.error("Error fetching data:", error));
